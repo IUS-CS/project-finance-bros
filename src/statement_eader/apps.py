d373f365bd0858f49ce348a8dfb422fc6811ad67
andermiah <andermiah@gmail.com>
2026-02-10 12:54:42 -0500

    Fix typo from statement_eader to statement_reader

--- src/statement_eader/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StatementEaderConfig(AppConfig):
-    name = 'statement_eader'
---
+++ src/statement_reader/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StatementReaderConfig(AppConfig):
+    name = 'statement_reader'
