4770ff267360902266789d054dc3dff2c9284521
andermiah <andermiah@gmail.com>
2026-02-28 17:34:12 -0500

    Added tests and changed Django Github Action to properly run the tests

--- src/statement_reader/tests.py
+++ src/statement_reader/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Transaction
+import datetime
 
 # Create your tests here.
+class TransactionInputViewTest(TestCase):
+    def test_transaction_page_loads(self):
+        response = self.client.get('/input')
+        self.assertEqual(response.status_code, 200)
+
+    def test_transaction_post_saves_to_database(self):
+        response = self.client.post('/input', {'vendor_name': 'Speedway',
+                                              'date': datetime.date.today(),
+                                              'amount': '42.00'})
+        self.assertEqual(Transaction.objects.count(), 1)
