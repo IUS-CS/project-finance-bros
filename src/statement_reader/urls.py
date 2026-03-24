6592a0d50e6ed00ced77828a698247e6d3aed0c4
aarcmis <aaronmiskuf@outlook.com>
2026-03-24 13:33:35 -0400

    Quick fix for accidentally adding a bracket

--- src/statement_reader/urls.py
+++ src/statement_reader/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path("list", views.transactions_list, name="transactions_list"),
     path("input", views.transactions_input),
     path("reader", views.reader),
-    path("upload", views.upload_file)
-]
+    path("upload", views.upload_file),
     path ("edit/<int:pk>/", views.transactions_edit_form, name="transaction_edit_form"),
     path ("delete/<int:pk>/", views.transactions_delete_item, name="transaction_delete_item")
 ]
